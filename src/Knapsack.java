fa2bc8f47d94820cbc1d6b7060f8b1ad6841e208
oliviamcrobbie <oliviamcrobbie@192.168.20.12>
2018-09-21 12:46:29 +1200

    finished

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -2,52 +2,63 @@
 public class Knapsack {
 
 	private int[] value;
-	private int[] weights; 
+	private int[] weights;
 	private int W;
 	private int n;
-	
-	public Knapsack(int[] value, int[] weights, int w, int n) {
+	private int solution;
+
+	public Knapsack(int[] value, int[] weights, int wt, int n) {
 		this.value = value;
 		this.weights = weights;
-		this.W = w;
-		this.n= n;
-		int solution = knapsackAlgorithm();
-		System.out.println(solution);
-	}
+		this.W = wt;
+		this.n = n;
 	
-	public int knapsackAlgorithm() {
-		
-		int[][] m = new int[n+1][W+1];
-		
-		for(int j = 0; j <= W; j++) {
+		knapsackAlgorithm();
+	}
+
+	public void knapsackAlgorithm() {
+
+		int[][] m = new int[n + 1][W + 1];
+
+		for (int j = 0; j <= W; j++) {
 			m[0][j] = 0;
 		}
-		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 0; j <=W; j++) {
-				if(weights[i] > j) {
-					m[i][j] = m[i-1][j];
-				}
-				else {
-					m[i][j] = max(m[i-1][j], m[i-1][j-weights[i]] + value[i]);
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j <= W; j++) {
+				if (weights[i] > j) {
+					m[i][j] = m[i - 1][j];
+				} else {
+					m[i][j] = max(m[i - 1][j], m[i - 1][j - weights[i]] + value[i]);
 				}
 			}
 		}
-		
-		
-		return m[n][W];
+
+		int sol = m[n][W];
+		System.out.println(sol);
+		int w = W;
+		for (int i = n; i > 0 && sol > 0; i--) {
+			if (sol != m[i - 1][w]) {
+				System.out.println(weights[i] + " ");
+
+				sol = sol - value[i];
+				w = w - weights[i];
+			}
+		}
+
 	}
+
 	
 	public int max(int a, int b) {
-		if(a >= b) return a;
-		else return b;
+		if (a >= b)
+			return a;
+		else
+			return b;
 	}
-	
-	public int[] copyArray(int[] A, int[] B) {
-		for(int i = 0; i <= B.length; i++) {
-			A[i] = B[i];
-		}
-		
-		return A;
+
+	public int getSol() {
+		return solution;
+
 	}
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,95 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
+	private static int numberOfNTested = 11;
+	private static long[] times = new long[numberOfNTested];
+	private static int W = 2000;
+
+	private static int numTestsPerN = 5;
+
+	/**
+	 * args[0] = capacity
+	 * args[1] = number of items
+	 * values
+	 * weights
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int W = 2000;
-		int n = 6;
-		int[] weights = {0, 580, 1616, 1906, 1942, 50, 294};
-		int[] values = {0, 874, 620, 345, 369, 360, 470};
-		Knapsack ks = new Knapsack(values, weights ,W, n);
+
+		int W = Integer.parseInt(args[0]);
+		int n = Integer.parseInt(args[1]);
+		
+		int[] values = new int[n+1];
+		int[] weights = new int[n+1];
+		values[0] = 0; weights[0] = 0;
+		
+		for(int i = 2; i < n +2; i++) {
+			values[i-1] = Integer.parseInt(args[i]);
+		}
+		for(int i = n+2; i < 2*n + 2; i++) {
+			weights[i - (n+1)] = Integer.parseInt(args[i]);
+		}
+		
+		
+		if(weights.length != values.length) {
+			System.out.println("number of weights/values dont match");
+			System.exit(n);
+		}
+		
+		Knapsack k = new Knapsack(values, weights, W, n);
+		
+		/**
+		for (int t = 0; t < 50; t++) {
+			int count = 0;
+			for (int i = 10; i <= 1011; i += 100) {
+				long time = tests(i, W);
+				times[count]+=time;
+				count++;
+			}
+		}
+		
+		for(int i = 0; i < times.length; i++) {
+			times[i] = times[i]/50;
+		}
+
+		for (int i = 0; i < numberOfNTested; i++) {
+			System.out.println(times[i]);
+		}
+		*/
 	}
 
+	public static long tests(int n, int W) {
+		Random r = new Random();
+		long time = 0;
+		for (int i = 0; i < numTestsPerN; i++) {
+
+			// generate values
+			int[] values = new int[n + 1];
+			values[0] = 0;
+			for (int a = 1; i <= n; i++) {
+				values[a] = r.nextInt(1000);
+			}
+
+			// generate weights
+			int[] weights = new int[n + 1];
+			weights[0] = 0;
+			for (int a = 1; i <= n; i++) {
+				weights[a] = r.nextInt(W);
+			}
+			long startTime = System.nanoTime();
+			int solution = new Knapsack(values, weights, W, n).getSol();
+			long endTime = System.nanoTime();
+			long totalTime = endTime - startTime;
+
+			time += totalTime;
+		}
+
+		long avgTime = time / numTestsPerN;
+		return avgTime;
+
+	}
 }
